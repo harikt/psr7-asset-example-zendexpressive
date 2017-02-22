eb4f179330ac1c9862ca69b6df4412bc8577f135
Hari KT <kthari85@gmail.com>
2017-02-22 11:15:33 +0530

    Update container configuration

--- config/container.php
+++ config/container.php
@@ -10,34 +10,11 @@ require_once __DIR__ . '/ExpressiveAuraDelegatorFactory.php';
 // Load configuration
 $config = require __DIR__ . '/config.php';
 
-// module loader
-$configClasses = [
-    Hkt\Psr7AssetExample\Config\Common::class,
-    App\Config\Common::class,
-    new ExpressiveAuraConfig(is_array($config) ? $config : []),
-];
-
 // Build container
 $builder = new ContainerBuilder();
-$container = $builder->newInstance();
-$configs = [];
-foreach ($configClasses as $configClass) {
-    if (is_string($configClass)) {
-        $configClass = new $configClass;
-    }
 
-    if (! $configClass instanceof ContainerConfigInterface) {
-        continue;
-    }
-
-    $configClass->define($container);
-    $configs[] = $configClass;
-}
-
-$container->lock();
-
-foreach ($configs as $configuration) {
-    $configuration->modify($container);
-}
-
-return $container;
+return $builder->newConfiguredInstance([
+    new ExpressiveAuraConfig(is_array($config) ? $config : []),
+    Hkt\Psr7AssetExample\Config\Common::class,
+    App\Config\Common::class,
+]);
