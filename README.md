9ba7d25f7297ef9b3591e080d6cbe59fd9b74361
Hari KT <kthari85@gmail.com>
2017-03-18 00:03:43 +0530

    Add asset cache

--- README.md
+++ README.md
@@ -20,3 +20,12 @@ $assetLocator = $di->get('Hkt\Psr7Asset\AssetLocator');
 $rootPath = dirname(dirname(dirname(__DIR__)));
 $assetLocator->set('hkt/psr7-asset-example/images/white-image.png', $rootPath . '/public/psr7-asset-example/white-image.png');
 ```
+
+## Caching Assets
+
+Assets from different `vendor/package` can be cached to the document root,
+so it can be served by the server.
+
+```bash
+php bin/console.php hkt:psr7-asset:cache ./public
+```
--- src/App/Config/Common.php
+++ src/App/Config/Common.php
@@ -10,19 +10,19 @@ class Common implements ContainerConfigInterface
     public function define(Container $di)
     {
         $di->set('Interop\Http\Factory\ResponseFactoryInterface', $di->lazyNew('Http\Factory\Diactoros\ResponseFactory'));
-        $di->params['Hkt\Psr7Asset\AssetResponder']['responseFactory'] = $di->lazyGet('Interop\Http\Factory\ResponseFactoryInterface');
-        $di->set('Hkt\Psr7Asset\Router', $di->lazyNew('Hkt\Psr7Asset\Router'));
-        $di->set('Hkt\Psr7Asset\AssetLocator', $di->lazyNew('Hkt\Psr7Asset\AssetLocator'));
-
-        $di->params['Hkt\Psr7Asset\AssetService']['locator'] = $di->lazyGet('Hkt\Psr7Asset\AssetLocator');
-
-        $di->params['Hkt\Psr7Asset\AssetAction'] = array(
-            'domain' => $di->lazyNew('Hkt\Psr7Asset\AssetService'),
-            'responder' => $di->lazyNew('Hkt\Psr7Asset\AssetResponder'),
-            'router' => $di->lazyGet('Hkt\Psr7Asset\Router'),
-        );
-
-        $di->set('Hkt\Psr7Asset\AssetAction', $di->lazyNew('Hkt\Psr7Asset\AssetAction'));
+        // $di->params['Hkt\Psr7Asset\AssetResponder']['responseFactory'] = $di->lazyGet('Interop\Http\Factory\ResponseFactoryInterface');
+        // $di->set('Hkt\Psr7Asset\Router', $di->lazyNew('Hkt\Psr7Asset\Router'));
+        // $di->set('Hkt\Psr7Asset\AssetLocator', $di->lazyNew('Hkt\Psr7Asset\AssetLocator'));
+        //
+        // $di->params['Hkt\Psr7Asset\AssetService']['locator'] = $di->lazyGet('Hkt\Psr7Asset\AssetLocator');
+        //
+        // $di->params['Hkt\Psr7Asset\AssetAction'] = array(
+        //     'domain' => $di->lazyNew('Hkt\Psr7Asset\AssetService'),
+        //     'responder' => $di->lazyNew('Hkt\Psr7Asset\AssetResponder'),
+        //     'router' => $di->lazyGet('Hkt\Psr7Asset\Router'),
+        // );
+        //
+        // $di->set('Hkt\Psr7Asset\AssetAction', $di->lazyNew('Hkt\Psr7Asset\AssetAction'));
         $di->set('TwigEnvironment', $di->lazyNew('Zend\Expressive\Twig\TwigEnvironmentFactory'));
         $di->set('Zend\Expressive\Twig\TwigRendererFactory', $di->lazyNew('Zend\Expressive\Twig\TwigRendererFactory'));
         $di->set('Zend\Expressive\Template\TemplateRendererInterface', $di->lazyGetCall('Zend\Expressive\Twig\TwigRendererFactory', '__invoke', $di));
@@ -41,8 +41,8 @@ class Common implements ContainerConfigInterface
         $router->addRoute($route);
         $router->addRoute(new Route('/', 'Hkt\Psr7AssetExample\Middleware\Welcome', ['GET'], 'hkt/psr7-asset-example:welcome'));
         // Try modifying the asset to a different url
-        // $assetLocator = $di->get('Hkt\Psr7Asset\AssetLocator');
-        // $rootPath = dirname(dirname(dirname(__DIR__)));
-        // $assetLocator->set('hkt/psr7-asset-example/images/white-image.png', $rootPath . '/public/psr7-asset-example/white-image.png');
+        $assetLocator = $di->get('Hkt\Psr7Asset\AssetLocator');
+        $rootPath = dirname(dirname(dirname(__DIR__)));
+        $assetLocator->set('hkt/psr7-asset-example/images/white-image.png', $rootPath . '/public/zf-logo.png');
     }
 }
